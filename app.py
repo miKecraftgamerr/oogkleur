cb11bf4fb15c105feb2d52ec474d22d9e697801a
miKecraftgamerr <160782657+miKecraftgamerr@users.noreply.github.com>
2024-02-21 21:08:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def genetische_berekeningen(parent1, parent2, inLaw1, inLaw2):
-    # Doe hier je genetische berekeningen op basis van de opgegeven tabel
-    kansOpBruin = 0
-    kansOpGroen = 0
-    kansOpBlauw = 0
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    # Voer hier je eigen genetische berekeningen uit
-
-    return {'kansOpBruin': kansOpBruin, 'kansOpGroen': kansOpGroen, 'kansOpBlauw': kansOpBlauw}
-
-@app.route('/calculate_eye_colors', methods=['GET'])
-def calculate_eye_colors():
-    parent1 = request.args.get('parent1')
-    parent2 = request.args.get('parent2')
-    inLaw1 = request.args.get('inLaw1')
-    inLaw2 = request.args.get('inLaw2')
-
-    result
+if __name__ == '__main__':
+    app.run(debug=True)
